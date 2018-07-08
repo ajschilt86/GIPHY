295e16aa914d15712ed7b076235ebee2dad46815
ajschilt <ajschilt86@gmail.com>
2018-07-07 20:21:48 -0500

    start stop working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -29,27 +29,44 @@ $(document).ready(function () {
         console.log(queryURL);
         console.table(response);
 
-        for (var i = 0; i < 3; i++) {
-          $(".gifs").append("<img class='imgSize' src='" + response.data[i].images.fixed_height.url + "'>");
+        for (var i = 0; i < 5; i++) {
+
+          $(".gifs").append("<img src='" + response.data[i].images.fixed_height_still.url + "' data-still='" + response.data[i].images.fixed_height_still.url + "' data-animate='" + response.data[i].images.fixed_height.url + "' data-state='still' class='imgSize'>");
+
           $(".gifs").append("<p> Rating: " + response.data[i].rating + "</p>");
+
+
+          $(".imgSize").click(function () {
+
+
+
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
           var q = 1;
         }
 
         //adding 10
-        $(".buttons button").click(function () {
-          q += 1;
-          var g = q * 3;
-          if (g < 25) {
-            for (i; i < g; i++) {
-              $(".gifs").append("<img class='imgSize' src='" + response.data[i].images.fixed_height.url + "'>");
-              $(".gifs").append("<p> Rating: " + response.data[i].rating + "</p>");
-            }
-          }
-          else {
-            return false;
-          }
+        // $(".buttons button").click(function () {
+        //   q += 1;
+        //   var g = q * 3;
+        //   if (g < 25) {
+        //     for (i; i < g; i++) {
+        //       $(".gifs").append("<img class='imgSize' src='" + response.data[i].images.fixed_height_still.url + "'>");
+        //       $(".gifs").append("<p> Rating: " + response.data[i].rating + "</p>");
+        //     }
+        //   }
+        //   else {
+        //     return false;
+        //   }
 
-        });
+        // });
       });
     });
   }
